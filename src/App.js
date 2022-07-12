ab94dfb70b6d1b677b76a821ae951a400b309454
Lillian Yang <nopeyangli@163.com>
2022-07-12 21:48:30 +1000

    React.Fragment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import classes from './App.module.css';
+import Out from "./Out";
 
 const App = () => {
-  return <div>i am an App</div>;
+
+
+  return <React.Fragment>
+      <div>Module1</div>
+      <div>Module2</div>
+      <div>Module3</div>
+    </React.Fragment>;
 };
 
 export default App;
+  
