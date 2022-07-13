babec2a6daebf4492d7c50131ef5be5d00899f8c
Lillian Yang <nopeyangli@163.com>
2022-07-14 00:58:16 +1000

    fontAwsome

--- src/components/Meals/Meal/UI/Counter/Counter.js
+++ src/components/Meals/Meal/UI/Counter/Counter.js
@@ -1,5 +1,7 @@
 import React, { Fragment } from "react";
 import classes from "./Counter.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
 
 const Counter = (props) => {
   return (
@@ -7,13 +9,15 @@ const Counter = (props) => {
       {props.amount && props.amount !== 0 ? (
         <>
           <button className={classes.Sub}>
-            <span>-</span>
+            <FontAwesomeIcon icon={faMinus} />
           </button>
           <span className={classes.count}>{props.amount}</span>
         </>
       ) : null}
 
-      <button className={classes.Add}>+</button>
+      <button className={classes.Add}>
+        <FontAwesomeIcon icon={faPlus} />
+      </button>
     </div>
   );
 };
