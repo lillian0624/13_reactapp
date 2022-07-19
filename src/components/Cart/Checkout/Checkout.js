161559f4f5241b4cdb0de52309783952046cc73e
Lillian Yang <nopeyangli@163.com>
2022-07-19 19:08:31 +1000

    Checkout Header and Footer

--- src/components/Cart/Checkout/Checkout.js
+++ src/components/Cart/Checkout/Checkout.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ReactDOM  from 'react-dom';
 import classes from './Checkout.module.css'
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import CartContext from '../../../store/CartContext';
  
 const CheckoutRoot =  document.getElementById('checkout-root');
 
 const Checkout = (props) => {
+
+const ctx = useContext(CartContext);
+
     return ReactDOM.createPortal(
     <div className={classes.Checkout}>
         <div className={classes.Close}>
             <FontAwesomeIcon 
             onClick={()=>props.onHide()}
             icon={faXmark}/>
+        </div>
+        <div className={classes.MealsDesc}>
+        <header className={classes.Header}>
+            <h2 className={classes.Title}>order details</h2>
+        </header>
+        <div> list</div>
+        <footer className={classes.Footer}>
+            <p className={classes.TotalPrice}>{ctx.totalPrice}</p>
+        </footer>
         </div>
     </div>,
     CheckoutRoot);
---
+++ src/components/Cart/Checkout/CheckoutItem/CheckoutItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './CheckoutItem.module.css';
+
+const CheckoutItem = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CheckoutItem;
